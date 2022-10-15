05a53b4ac6000b832a007c7082d17cea73e546df
Asiye <gokalpasiye@gmail.com>
2022-10-15 23:14:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const Datastore = require("nedb");
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+
+
+const app = express();
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("public"));
+app.use(express.json());
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+app.get("/api", (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      res.end();
+      return;
+    }
+    res.json(data);
+  });
+});
+
+app.post("/api", (request, response) => {
+  console.log("I got a request!");
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
+});
+
+
+app.get("/weather/:latlon",async (req,res) => {
+  
+  const latlon = req.params.latlon.split(',')
+  const lat=latlon[0]
+  const lon = latlon[1]
+  const api_key = process.env.API_KEY
+const weatherUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+//const apiUrl=`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b5af33f75912e63bff0bf29c9d0a3822`
+const weatherResponse = await fetch(weatherUrl)
+const weatherData = await weatherResponse.json()
+
+const aqUrl = `https://api.openaq.org/v1/latest?coorinates = ${lat},${lon}`
+const aqResponse = await fetch(aqUrl)
+const aqData = await aqResponse.json()
+const data = {
+  weather : weatherData,
+  ai_quality : aqData
+}
+
+res.json(data)
+})
